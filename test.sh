586fae0074631fc39a6a9e2b30d8afe56173f459
Muhamad Azamy <muhamad@incubaid.com>
2021-06-10 11:45:02 +0200

    WIP: improvment on the kernel
    Also include a basic kernel config file

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@ init=$2
 sudo virtiofsd --socket-path=/tmp/root.socket -o source=${root} -o cache=none &
 
 if [ -z "${init}" ]; then
-    init='init=/bin/sh'
+    init='init=/sbin/zinit "init"'
 fi
 
 exec sudo cloud-hypervisor \
-    --kernel output/vmlinuz-linux \
+    --kernel output/bzImage \
     --initramfs output/initramfs-linux.img \
     --console off \
     --serial tty \
     --cpus boot=1 \
-    --memory size=1024M,shared=on \
+    --memory size=300M,shared=on \
     --fs tag=/dev/root,socket=/tmp/root.socket  \
     --net tap=cont0,ip=192.168.123.100,mask=255.255.255.0 \
     --cmdline "console=ttyS0 rootfstype=virtiofs root=/dev/root rw ${init}" \
